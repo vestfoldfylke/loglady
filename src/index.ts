7a0b1e40ac1eaa396b67cc6ecd7cdd8d400a7db5
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-10-31 12:44:14 +0100

    Bugfix: flush didn't work because it was looking at an array of TrackedPromise instead of an array of Promise

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,8 @@ export namespace logger {
    * Should be called before application exits or session is finished, to ensure all log messages have been processed (either successfully or with an error)
    */
   export async function flush(): Promise<void> {
-    await Promise.allSettled(_queue);
-    _queue.length = 0;
+    await Promise.allSettled(_queue.map((trackedPromise: TrackedPromise) => trackedPromise.promise));
+    _queue.splice(0, _queue.length, ..._queue.filter((trackedPromise: TrackedPromise) => !trackedPromise.isSettled));
   }
 
   /**
